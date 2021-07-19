0dcf2185a8ef966a9e6c031c8ba8787ef5c85507
Phil Varner <philvarner@gmail.com>
2021-07-19 10:44:15 -0400

    install

--- install.sh
+++ install.sh
@@ -4,8 +4,9 @@ set -x # print each command before executing
 
 echo "Installing aws cdk (npm)"
 npm install -g aws-cdk
-# Note: zsh users need to use ""
+
 echo "Installing python packages (pip)"
 pip install -e ".[deploy]"
 
+echo "Deploying to AWS"
 cdk deploy --all --require-approval never
