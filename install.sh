77fed551226257e87e6708cb6a68c8e6e1267b0b
Phil Varner <philvarner@gmail.com>
2021-07-19 14:01:36 -0400

    npm debugging

--- install.sh
+++ install.sh
@@ -2,11 +2,16 @@
 set -Eeuo pipefail
 set -x # print each command before executing
 
+npm --version
+
+npm pack --unsafe-perm
+
 echo "Installing aws cdk (npm)"
 npm install -g aws-cdk
 
 echo "Installing python packages (pip)"
 pip install -e ".[deploy]"
+
 
 echo "Deploying to AWS"
 cdk deploy --all --require-approval never -vvv
